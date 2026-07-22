8556ac6670df48dccb8225995cd54d0159d8e000
Aluno em Vocação Nicolas santana <nicolasdml02@gmail.com>
2026-07-22 13:35:06 -0300

    upgrade_txt_alterado
    nomes alterados

--- modulo06-nicolasantana.py
+++ modulo06-nicolasantana.py
@@ -10,7 +10,7 @@
 leitura_arquivo = open('arquivo_leitura_nicolasantana.txt', 'r', encoding="utf-8")
 
 #2. Lê todo o conteúdo do arquivo
-conteudo_arquivo = leitura_arquivo.readlines()
+conteudo_arquivo = leitura_arquivo.read()
 
 #3. Exibe o conteúdo na tela
 print(conteudo_arquivo)
